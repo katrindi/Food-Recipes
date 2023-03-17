03b8e764c8963c7a02a11fdb2f75f55ae4818309
katrindi <107497899+katrindi@users.noreply.github.com>
2023-03-17 10:23:19 +0200

    Add files via upload

---
+++ MyCookWeb - Copy/HTML-CSS-JS/app.js
@@ -0,0 +1,81 @@
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+const Accordionitemheader = document.querySelectorAll(".Accordion-item-header");
+Accordionitemheader.forEach(Accordionitemheader => {
+  Accordionitemheader.addEventListener("click",event => {
+    Accordionitemheader.classList.toggle("active");
+    const Accordionitembody = Accordionitemheader.nextElementSibling;
+    if(Accordionitemheader.classList.contains("active")){
+      Accordionitembody.style.maxHeight = Accordionitembody.scrollHeight + "px";
+    }
+    else{
+      Accordionitembody.style.maxHeight = 0;
+    }
+  })
+});
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
